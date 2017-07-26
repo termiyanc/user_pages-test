a69786a2a462e6264e7502d32b9ad6b802b707e0
Eduard <eduard.termiyanc@mail.ru>
2017-07-26 14:22:57 +0300

    Удаляю неиспользуемый класс.

--- views/Main/main.php
+++ views/Main/main.php
@@ -9,7 +9,7 @@ if ($pages) {
     foreach ($pages as $page) {
     ?>
     <tr>
-        <td class="first-cell"><a href='<?=$this->url('pages', 'view', [$page['id']])?>' class="page-link"><?=$page['title']?></a></td>
+        <td><a href='<?=$this->url('pages', 'view', [$page['id']])?>' class="page-link"><?=$page['title']?></a></td>
         <td><a href='<?=$this->url('pages', 'change', [$page['id']])?>' class="page-action">Изменить</a></td>
         <td><a href='<?=$this->url('pages', 'delete', [$page['id']])?>' class="page-action">Удалить</a></td>
     </tr>
